a9062b945e8ba8a1608e83994e55c16d570548f4
Senull0 <fnull@tuta.io>
2023-11-09 00:50:07 +0200

    quick fixes

--- src/Pages/CreateAccount/SignUp.js
+++ src/Pages/CreateAccount/SignUp.js
@@ -37,67 +37,74 @@ export const SignUp = (props) => {
           <form className={styles.form} onSubmit={submitHandler}>
             <img 
               src = {new_account}
-              style={{ width: '25%', height: 'auto', paddingTop: "15%", paddingBottom: "10%"}}
+              style={{ width: '25%', height: 'auto', paddingTop: "8%", paddingBottom: "10%"}}
             />
 
             {/* Email */}
-            <label className={styles.label} htmlFor="email">Email</label>
-            <input
-              className={styles.input_box}
-              id = "email"
-              value={email}
-              onChange={event => setEmail(event.target.value)}
-              name="email"
-              type="email"
-            />
+            <div className= {styles.input_boxp}>
+              <label className={styles.label} htmlFor="email">Email</label><br/>
+              <input
+                className={styles.input_box}
+                id = "email"
+                value={email}
+                onChange={event => setEmail(event.target.value)}
+                name="email"
+                type="email"
+              />
+            </div>
 
 
             {/* Full name */}
-            <label className={styles.label} htmlFor="name">Full name</label>
-            <input
-              className={styles.input_box}
-              id = "name"
-              value={name}
-              onChange={InputSanitazier_name}
-              name="name"
-              type="text"
-            />
-
+            <div className= {styles.input_boxp}>
+              <label className={styles.label} htmlFor="name">Full name</label><br/>
+              <input
+                className={styles.input_box}
+                id = "name"
+                value={name}
+                onChange={InputSanitazier_name}
+                name="name"
+                type="text"
+              />
+            </div>
 
             {/* City */}
-            <label className={styles.label} htmlFor="City">City</label>
-            <input
-              className={styles.input_box}
-              id = "city"
-              value={city}
-              onChange={InputSanitazier_city}
-              name="city"
-              type="text"
-            />
-
-
-            {/* Password */}
-            <label className={styles.label} htmlFor="postalCode">Postal Code</label>
-            <input
-              className={styles.input_box}
-              id = "postalCode"
-              value={postalCode}
-              onChange={event => setPostalCode(event.target.value)}
-              name="postalCode"
-              type="number"
-            />
-
+            <div className= {styles.input_boxp}>
+              <label className={styles.label} htmlFor="City">City</label><br/>
+              <input
+                className={styles.input_box}
+                id = "city"
+                value={city}
+                onChange={InputSanitazier_city}
+                name="city"
+                type="text"
+              />
+            </div>
 
             {/* Password */}
-            <label className={styles.label} htmlFor="password">Password</label>
-            <input
-              className={styles.input_box}
-              id = "password"
-              value={password}
-              onChange={event => setPassword(event.target.value)}
-              name="password"
-              type="password"
-            />
+            <div className= {styles.input_boxp}>
+              <label className={styles.label} htmlFor="postalCode">Postal Code</label><br/>
+              <input
+                className={styles.input_box}
+                id = "postalCode"
+                value={postalCode}
+                onChange={event => setPostalCode(event.target.value)}
+                name="postalCode"
+                type="number"
+              />
+            </div>
+
+            {/* Password */}
+            <div className= {styles.input_boxp}>
+              <label className={styles.label} htmlFor="password">Password</label><br/>
+              <input
+                className={styles.input_box}
+                id = "password"
+                value={password}
+                onChange={event => setPassword(event.target.value)}
+                name="password"
+                type="password"
+              />
+            </div>
 
         <button className={styles.create_button}>Create</button>
         <Link to={`/login`} className={styles.account_already}>Already have an account?</Link>
